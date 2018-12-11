bbdaf13ebd5249a702808db023ac1041b852cebc
LilianKalkman <liliankalkman@hotmail.com>
2018-12-11 14:33:15 +0100

    added routes

--- src/views/Join.vue
+++ src/views/Join.vue
@@ -1,7 +1,14 @@
 <template>
   <v-container>
     <v-layout>
-      <h1>About Page</h1>
+      <h1>Join Page</h1>
     </v-layout>
   </v-container>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,7 +1,14 @@
 <template>
   <v-container>
     <v-layout>
-      <h1>About Page</h1>
+      <h1>Menu Page</h1>
     </v-layout>
   </v-container>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
