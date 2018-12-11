f60d6ced014d3d79d8a69305a1f26c577d5bcb14
LilianKalkman <liliankalkman@hotmail.com>
2018-12-11 11:03:56 +0100

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <app-navigation></app-navigation>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import AppNavigation from "@/components/AppNavigation.vue";
 
 export default {
-  name: "home",
   components: {
-    HelloWorld
+    AppNavigation
   }
 };
 </script>
